17bf56ec9e7ef931d38cf0bca8e5caed47e9fdab
Ronald Chmara <ronabop@gmail.com>
2015-10-27 13:07:29 -0700

    Update version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,9 @@
 #
 
 # Microsoft Visual C++ 2010  x86 Redistributable
-default['vc2010']['x86']['productname'] = 'Microsoft Visual C++ 2010  x86 Redistributable - 10.0.30319'
-default['vc2010']['x86']['url'] = 'http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe'
+default['vc2010']['x86']['productname'] = 'Microsoft Visual C++ 2010  x86 Redistributable - 10.0.40219'
+default['vc2010']['x86']['url'] = 'http://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe'
 
 # Microsoft Visual C++ 2010  x64 Redistributable
-default['vc2010']['x64']['productname'] = 'Microsoft Visual C++ 2010  x64 Redistributable - 10.0.30319'
-default['vc2010']['x64']['url'] = 'http://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe'
+default['vc2010']['x64']['productname'] = 'Microsoft Visual C++ 2010  x64 Redistributable - 10.0.40219'
+default['vc2010']['x64']['url'] = 'http://download.microsoft.com/download/A/8/0/A80747C3-41BD-45DF-B505-E9710D2744E0/vcredist_x64.exe'
